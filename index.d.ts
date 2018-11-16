22a12ba1470ef0ed20232b7db12da0a9e109a782
Pedro Barbiero <pedrobarbiero@gmail.com>
2018-11-16 11:41:46 -0200

    Add typescript support. Closes #19.

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare module 'react-native-languages' {
+  interface EventData {
+    language: string
+    languages: string[]
+  }
+
+  type EventHandler = (eventData: EventData) => any
+
+  interface RNLanguagesModule {
+    readonly language: string
+    readonly languages: string[]
+    addEventListener: (type: 'change', handler: EventHandler) => void
+    removeEventListener: (type: 'change', handler: EventHandler) => void
+  }
+
+  export const language: string
+  export const languages: string[]
+
+  let Module: RNLanguagesModule
+
+  export default Module
+}
