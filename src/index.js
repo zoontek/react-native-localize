708b114a82dfaa087a943659f073aa60db637d56
Mathieu Acthernoene <zoontek@gmail.com>
2020-01-26 13:03:10 +0100

    Add development linking warning

--- src/index.js
+++ src/index.js
@@ -15,6 +15,16 @@ import type {
 
 const { RNLocalize } = NativeModules;
 
+if (__DEV__ && RNLocalize == null) {
+  throw new Error(`react-native-localize: NativeModule.RNLocalize is null. To fix this issue try these steps:
+• Run \`react-native link react-native-localize\` in the project root.
+• Rebuild and re-run the app.
+• If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app. You may also need to re-open Xcode to get the new pods.
+• Check that the library was linked correctly when you used the link command by running through the manual installation instructions in the README.
+* If you are getting this error while unit testing you need to mock the native module. Follow the guide in the README.
+If none of these fix the issue, please open an issue on the Github repository: https://github.com/react-native-community/react-native-localize`);
+}
+
 let constants: LocalizationConstants = RNLocalize.initialConstants;
 
 export function getCalendar(): Calendar {
