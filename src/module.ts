28257024f9b9f3a570cbd9a5f691d96dab36a8f8
Mathieu Acthernoene <zoontek@gmail.com>
2020-11-14 16:29:53 +0100

    Fix dot

--- src/module.ts
+++ src/module.ts
@@ -15,8 +15,8 @@ if (__DEV__) {
 • Rebuild and re-run the app.
 • If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app. You may also need to re-open Xcode to get the new pods.
 • Check that the library was linked correctly when you used the link command by running through the manual installation instructions in the README.
-* If you are getting this error while unit testing you need to mock the native module. Follow the guide in the README.
-If none of these fix the issue, please open an issue on the Github repository: https://github.com/zoontek/react-native-localize`);
+• If you are getting this error while unit testing you need to mock the native module. Follow the guide in the README.
+• If none of these fix the issue, please open an issue on the Github repository: https://github.com/zoontek/react-native-localize`);
   }
 }
 
