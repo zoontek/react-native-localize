4aaeb4dfc557797dfea4168f63261c4b60a96696
Mathieu Acthernoene <zoontek@gmail.com>
2021-08-16 11:42:29 +0200

    Update react-native to 0.64

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,13 +1,12 @@
-
-const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const path = require("path");
+const blacklist = require("metro-config/src/defaults/blacklist");
 
 module.exports = {
   resolver: {
     blacklistRE: blacklist([
       // This stops "react-native run-windows" from causing the metro server to crash if its already running
       new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+        `${path.resolve(__dirname, "windows").replace(/[/\\]/g, "/")}.*`,
       ),
       // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
       /.*\.ProjectImports\.zip/,
@@ -17,7 +16,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
