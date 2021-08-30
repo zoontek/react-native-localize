5ef8b79a40389f58ba4a222cc24cfdcbac01fb37
Mathieu Acthernoene <zoontek@gmail.com>
2021-08-30 11:37:51 +0200

    Fix missing Java import

--- android/src/main/java/com/zoontek/rnlocalize/RNLocalizeModule.java
+++ android/src/main/java/com/zoontek/rnlocalize/RNLocalizeModule.java
@@ -19,6 +19,7 @@ import androidx.annotation.Nullable;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
