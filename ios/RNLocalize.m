ed9399027a617beba2809504c014a977bd7d045c
Mathieu Acthernoene <zoontek@gmail.com>
2021-09-03 19:23:07 +0200

    Remove emitters stubs on iOS

--- ios/RNLocalize.m
+++ ios/RNLocalize.m
@@ -187,12 +187,4 @@ RCT_EXPORT_MODULE();
   [self sendEventWithName:@"localizationDidChange" body:[self exportedConstants]];
 }
 
-RCT_EXPORT_METHOD(addListener : (NSString *)eventName) {
-  // Keep: Required for RN built in Event Emitter Calls.
-}
-
-RCT_EXPORT_METHOD(removeListeners : (NSInteger)count) {
-  // Keep: Required for RN built in Event Emitter Calls.
-}
-
 @end
