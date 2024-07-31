fc07048fa85754e7dafdeef98324fb8717296fb0
Władysław Raczek <wsw.raczek@gmail.com>
2024-07-31 18:29:57 +0200

    [#266] Expose additional jest mocks alongside "regular" mocks (#267)

---
+++ mock/jest.ts
@@ -0,0 +1,14 @@
+import * as mock from ".";
+
+export const getCalendar = jest.fn(mock.getCalendar);
+export const getCountry = jest.fn(mock.getCountry);
+export const getCurrencies = jest.fn(mock.getCurrencies);
+export const getLocales = jest.fn(mock.getLocales);
+export const getNumberFormatSettings = jest.fn(mock.getNumberFormatSettings);
+export const findBestLanguageTag = jest.fn(mock.findBestLanguageTag);
+export const getTemperatureUnit = jest.fn(mock.getTemperatureUnit);
+export const getTimeZone = jest.fn(mock.getTimeZone);
+export const uses24HourClock = jest.fn(mock.uses24HourClock);
+export const usesAutoDateAndTime = jest.fn(mock.usesAutoDateAndTime);
+export const usesAutoTimeZone = jest.fn(mock.usesAutoTimeZone);
+export const usesMetricSystem = jest.fn(mock.usesMetricSystem);
